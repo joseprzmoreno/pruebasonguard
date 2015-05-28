897d46494710714798052e6c79108e48dc7c4698
Jose Perez Moreno <jos.pe.mo@gmail.com>
2015-05-29 01:58:18 +0200

    ----

--- OnGuardApp150528j/src/es/iesazarquiel/onguarapp/controllers/MainJefeViewController.java
+++ OnGuardApp150528j/src/es/iesazarquiel/onguarapp/controllers/MainJefeViewController.java
@@ -3,15 +3,26 @@ package es.iesazarquiel.onguarapp.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.Pane;
 
 public class MainJefeViewController {
+	
+	//dice al boton OK lo que tiene que hacer
+	private boolean activarVerHorarios = false;
+	private boolean activarComunicarFaltas = false;
+	private boolean activarVerFaltas = false;
+	private boolean activarVerGuardias = false;
+	private boolean activarAsignarGuardias = false;
+	private boolean activarFirmarGuardia = false;
+    private boolean activarGenerarInformeGuardias = false;
+    private boolean activarGenerarInformeFaltas = false;
+    
 
     @FXML
     private ResourceBundle resources;
@@ -59,13 +70,25 @@ public class MainJefeViewController {
     private Button informeFaltas;
 
     @FXML
-    private Label mostrarsaludo;
+    private DatePicker fecha1;
 
     @FXML
-    void saludar() {
-    	mostrarsaludo.setText("hola");
+    private DatePicker fecha2;
 
-    }
+    @FXML
+    private Button botonok;
+
+    @FXML
+    private ComboBox<?> listaFaltas;
+
+    @FXML
+    private ComboBox<?> listaHoras;
+
+    @FXML
+    private ComboBox<?> listaProfes;
+
+    @FXML
+    private Pane ContenedorSuperior;
 
     @FXML
     void initialize() {
@@ -82,7 +105,192 @@ public class MainJefeViewController {
         assert informes != null : "fx:id=\"informes\" was not injected: check your FXML file 'MainJefeView.fxml'.";
         assert informeGuadias != null : "fx:id=\"informeGuadias\" was not injected: check your FXML file 'MainJefeView.fxml'.";
         assert informeFaltas != null : "fx:id=\"informeFaltas\" was not injected: check your FXML file 'MainJefeView.fxml'.";
-        assert mostrarsaludo != null : "fx:id=\"mostrarsaludo\" was not injected: check your FXML file 'MainJefeView.fxml'.";
+        assert fecha1 != null : "fx:id=\"fecha1\" was not injected: check your FXML file 'MainJefeView.fxml'.";
+        assert fecha2 != null : "fx:id=\"fecha2\" was not injected: check your FXML file 'MainJefeView.fxml'.";
+        assert botonok != null : "fx:id=\"botonok\" was not injected: check your FXML file 'MainJefeView.fxml'.";
+        assert listaFaltas != null : "fx:id=\"listaFaltas\" was not injected: check your FXML file 'MainJefeView.fxml'.";
+        assert listaHoras != null : "fx:id=\"listaHoras\" was not injected: check your FXML file 'MainJefeView.fxml'.";
+        assert listaProfes != null : "fx:id=\"listaProfes\" was not injected: check your FXML file 'MainJefeView.fxml'.";
+        assert ContenedorSuperior != null : "fx:id=\"ContenedorSuperior\" was not injected: check your FXML file 'MainJefeView.fxml'.";
 
     }
+    
+    /*
+     * private boolean activarVerHorarios = false;
+	private boolean activarComunicarFaltas = false;
+	private boolean activarVerFaltas = false;
+	private boolean activarVerGuardias = false;
+	private boolean activarAsignarFaltas = false;
+	private boolean activarFirmarGuardia = false;
+    private boolean activarGenerarInformeGuardias = false;
+    private boolean activarGenerarInformeFaltas = false;
+     */
+    
+    @FXML
+    void enviarTodoAlFondo() {
+    	botonok.toBack();
+    	listaProfes.toBack();
+    	listaHoras.toBack();
+    	listaFaltas.toBack();
+    	fecha1.toBack();
+    	fecha2.toBack();
+    }
+    
+    @FXML 
+    void botonVerHorarios() {
+    	
+    	enviarTodoAlFondo();
+    	
+    	botonok.toFront();
+    	listaProfes.toFront();
+    	activarVerHorarios = true;
+    }
+    
+    @FXML 
+    void botonComunicarFaltas() {
+    	
+    	enviarTodoAlFondo();
+    	
+    	botonok.toFront();
+    	listaProfes.toFront();
+    	listaHoras.toFront();
+    	fecha1.toFront();
+    	activarComunicarFaltas = true;
+    }
+    
+    @FXML
+    void botonVerFaltas() {
+    	enviarTodoAlFondo();
+    	
+    	botonok.toFront();
+    	listaProfes.toFront();
+    	fecha1.toFront();
+    	fecha2.toFront();
+    	
+    	activarVerFaltas = true;
+    }
+    
+    @FXML
+    void botonVerGuardias() {
+    	enviarTodoAlFondo();
+    	
+    	botonok.toFront();
+    	listaProfes.toFront();
+    	fecha1.toFront();
+    	fecha2.toFront();
+    	
+    	activarVerGuardias = true;
+    }
+    
+    @FXML
+    void botonAsignarGuardias() {
+    	enviarTodoAlFondo();
+    	
+        botonok.toFront();
+        listaFaltas.toFront();
+        listaProfes.toFront();
+    	
+    	activarAsignarGuardias = true;
+    }
+    
+    @FXML
+    void botonFirmarGuardia() {
+    	enviarTodoAlFondo();
+    	
+        botonok.toFront();
+        fecha1.toFront();
+        listaHoras.toFront();
+        listaProfes.toFront();
+    	
+    	activarFirmarGuardia = true;
+    }
+    
+    @FXML
+    void botonGenerarInformeGuardias() {
+    	enviarTodoAlFondo();
+    	
+    	botonok.toFront();
+    	fecha1.toFront();
+    	
+    	activarGenerarInformeGuardias = true;
+    }
+    
+    @FXML
+    void botonGenerarInformeFaltas() {
+    	enviarTodoAlFondo();
+    	
+    	botonok.toFront();
+    	fecha1.toFront();
+    	
+    	activarGenerarInformeFaltas = true;
+    }
+    
+    
+    @FXML
+    void mostrarResultados() {
+    	
+    	if (activarVerHorarios) {
+    		
+    		/* Método de Alejandro*/
+    		
+    		activarVerHorarios = false;
+    	}
+    	
+    	else if (activarComunicarFaltas) {
+    		
+    		/*Método de Cristina*/
+    		
+    		activarComunicarFaltas = false;
+    	}
+    	
+    	else if (activarVerFaltas) {
+    		
+    		/*Método de Carlos*/
+    		
+    		activarVerFaltas = false;
+    		
+    	}
+    	
+    	else if (activarVerGuardias) {
+    		
+    		/*Método de Carlos*/
+    		
+    		activarVerGuardias = false;
+    	}
+    	
+    	else if (activarAsignarGuardias) {
+    		
+    		/*Método de Cristina*/
+    		
+    		activarAsignarGuardias = false;
+    		
+    	}
+    	
+    	else if (activarFirmarGuardia) {
+    		
+    		/*Método de Ana*/
+    		
+    		activarFirmarGuardia = false;
+    	}
+    	
+    	else if (activarGenerarInformeGuardias) {
+    		
+    		/*Método de Jose*/
+    		
+    		activarGenerarInformeGuardias = false;
+    	}
+    	
+    	else if (activarGenerarInformeFaltas) {
+    		
+    		/*Método de Jose*/
+    		
+    		activarGenerarInformeFaltas = false;
+    		
+    	}
+    
+
+    	
+    }
+    
+    
 }
